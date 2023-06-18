844a13ddccffaf8e340df54306fe2e8e72864185
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-06-18 18:27:04 +0300

    сделана вторая задача 2-ого семинара

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+int user_num = 0;
+
+Console.Write("Введите трехзначное число: ");
+user_num = int.Parse(Console.ReadLine() ?? "");
+
+if(user_num > 999 || user_num < 100)
+{
+    Console.WriteLine($"Число {user_num} не является трехзначным");
+}
+else
+{
+    Console.WriteLine($"Вторая цифра числа {user_num} -> {(user_num / 10) % 10}");
+}
