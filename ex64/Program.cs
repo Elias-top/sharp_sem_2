df170bb488586409b8a5e8e6aaac3b805dfb7096
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-07-12 19:17:15 +0300

    Сделана задача 64 урока 9

---
+++ ex64/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int user_num = 0;
+
+user_num = GetNumberFromUser("Задайте число N", "Ошибка ввода");
+GetNaturalNumArray(user_num);
+
+static int GetNaturalNumArray(int num)
+{
+    if(num == 0)
+        return 1;
+
+    Console.Write($"{num}, ");
+
+    return GetNaturalNumArray(num - 1);
+}
+
+static int GetNumberFromUser (String msg, String msg_error)
+{
+    while(true)
+    {
+        Console.WriteLine(msg);
+        if(int.TryParse(Console.ReadLine(), out int user_number))
+            return user_number;
+        Console.Write(msg_error);
+    }
+}
